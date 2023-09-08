8160c36e6b6bc19db5b373ef5c6deb76ec98eb83
tyzrex <sulavbaral58@gmail.com>
2023-09-08 23:24:40 +0545

    type declaration;

---
+++ betterbikes-server/src/interfaces/auth.ts
@@ -0,0 +1,20 @@
+export interface IRegister {
+    email: string;
+    password: string;
+    name: string;
+    phone: string;
+    address: string;
+}
+
+export interface ILogin{
+    email: string;
+    password: string;
+}
+
+export interface IGoogleLogin{
+    email: string;
+    name: string;
+    oAuthProvider: string;
+    oAuthId: string;
+    profileImage: string;
+}
