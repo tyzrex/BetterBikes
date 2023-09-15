bd574c987d791e6a37df574c4c3d9982f116969a
tyzrex <sulavbaral58@gmail.com>
2023-09-15 20:54:48 +0545

    error handler

--- betterbikes-server/src/services/auth.services.ts
+++ betterbikes-server/src/services/auth.services.ts
@@ -1,6 +1,20 @@
-import {prisma} from "../config/prisma"
 
-export const checkAlreadyRegistered= async (email: string, phone?: string) => {
+import {prisma} from "../config/prisma"
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
+import { generateRefreshToken, generateToken } from "../utils/generateToken";
+import { IGoogleLogin, IRegister } from "../interfaces/auth";
+import { IRegisteredResponse, IRegisteredUser } from "../interfaces/user";
+import bcrypt from "bcrypt";
+
+const accessExpireTime = new Date(
+  Date.now() + 24 * 60 * 60 * 1000
+);
+
+const refreshExpireTime = new Date(
+  Date.now() + 24 * 7 * 60 * 60 * 1000
+);
+
+export const checkAlreadyRegistered= async (email: string, phone?: string): Promise<IRegisteredResponse>=> {
  try {
     const user = await prisma.user.findUnique({
       where: {
@@ -30,13 +44,152 @@ export const checkAlreadyRegistered= async (email: string, phone?: string) => {
         oAuthUser,
         status: true,
       };
-    } else {
-      return {
+    } 
+
+    return {
+        user: null,
+        oAuthUser: null,
         status: false,
       };
-    }
   }
     catch(err:any){
-        console.log(err)
+        throw err
     }
 }
+
+interface IRefreshToken {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+
+export const createUser = async (userData: IRegister) => {
+  try {
+    const { email, password, fullName, phone, address } = userData;
+        const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+   const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name: fullName,
+        phone,
+        address,
+      },
+    });
+    return user;
+  }
+  catch(err:any){
+throw err
+  }
+}
+
+export const createOauthUser = async (userData: IGoogleLogin) => {
+  try {
+    const { email, name, profileImage, oAuthId, oAuthProvider} = userData;
+    const oAuthUser = await prisma.oAuthUser.create({
+        data: {
+          email: email,
+          name: name,
+          profile_image: profileImage,
+          oAuth_provider: oAuthProvider,
+          oAuth_id: oAuthId,
+        },
+      });
+    const token = generateToken(oAuthUser.id);
+    const refreshToken = generateRefreshToken(oAuthUser.id);
+
+    const user = {
+      id: oAuthUser.id,
+      name: oAuthUser.name,
+      email: oAuthUser.email,
+      access_token: token,
+      refreshToken: refreshToken,
+      accessExpireTime: accessExpireTime,
+      refreshExpireTime: refreshExpireTime,
+    };
+    return user;
+  }
+  catch(err:any){
+throw err
+  }
+}
+
+export const loginWithCredentials = async (registeredUser: IRegisteredResponse, password: string) => {
+ try{
+    const isPasswordValid = await bcrypt.compare(
+    password,
+      registeredUser?.user?.password as string
+    );
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid Password");
+    }
+
+    const token = generateToken(registeredUser?.user?.id as string);
+    const refreshToken = generateRefreshToken(
+      registeredUser?.user?.id as string
+    );
+
+    const user = {
+      id: registeredUser?.user?.id,
+      name: registeredUser?.user?.name,
+      email: registeredUser?.user?.email,
+      access_token: token,
+      refreshToken: refreshToken,
+      accessExpireTime: accessExpireTime,
+      refreshExpireTime: refreshExpireTime,
+    };
+
+    return user;
+
+ }
+  catch(err:any){
+throw err
+  }
+}
+
+export const loginWithOAuth = async (registeredUser: IRegisteredResponse) => {
+  try{
+    const token = generateToken(registeredUser?.oAuthUser?.id as string);
+    const refreshToken = generateRefreshToken(
+      registeredUser?.oAuthUser?.id as string
+    );
+    const user = {
+      id: registeredUser?.oAuthUser?.id,
+      name: registeredUser?.oAuthUser?.name,
+      email: registeredUser?.oAuthUser?.email,
+      access_token: token,
+      refreshToken: refreshToken,
+      accessExpireTime: accessExpireTime,
+      refreshExpireTime: refreshExpireTime,
+    };
+
+    return user;
+  }
+  catch(err:any){
+throw err
+  }
+}
+
+
+export const getRefreshToken = async (token: string) => {
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as IRefreshToken;
+    const newAccessToken = generateToken(decodedToken.id);
+    console.log("refreshed token")
+    return newAccessToken;
+  } catch (err: any) {
+    if (err instanceof TokenExpiredError) {
+      // Handle expired token error here
+      console.error('Token has expired:', err.message);
+      // You can return an error response or take other actions as needed
+      throw new Error('Token has expired');
+    } else {
+      // Handle other JWT verification errors
+      console.error('JWT verification error:', err.message);
+      throw new Error('Failed to get token');
+    }
+  }
+};
---
+++ betterbikes-server/src/utils/errorType.ts
@@ -0,0 +1,55 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { TokenExpiredError } from "jsonwebtoken";
+import { z, ZodError } from "zod";
+
+const ErrorHandler = (err: any) => {
+  let statusCode = 500;
+  let message = "Something went wrong";
+
+  // Handle Prisma Known Request Errors
+  if (err instanceof PrismaClientKnownRequestError) {
+    switch (err.code) {
+      case "P2002":
+        statusCode = 409;
+        message = "There is a unique constraint violation.";
+        break;
+      default:
+        statusCode = 400;
+        message = err.message;
+        break;
+    }
+  }
+
+  // Handle Zod Errors
+  if (err instanceof ZodError) {
+    const formatZodErrors = (error: z.ZodError): string => {
+      const firstError = error.errors[0];
+      if (firstError) {
+        return firstError.message;
+      }
+      return "Validation failed";
+    };
+
+    statusCode = 422;
+    message = formatZodErrors(err);
+  }
+
+  // Handle Token Expired Error
+  if (err instanceof TokenExpiredError) {
+    statusCode = 401;
+    message = "Token has expired";
+  }
+
+  // Handle other errors (unhandled)
+  if (!statusCode) {
+    statusCode = 500;
+    message = "Internal Server Error";
+  }
+
+  return {
+    statusCode,
+    message,
+  };
+};
+
+export default ErrorHandler;
