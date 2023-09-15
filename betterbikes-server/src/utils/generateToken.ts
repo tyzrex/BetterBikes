ed7a16569c6b9fffcf7f8bdc2702ac96a65b2f0c
tyzrex <sulavbaral58@gmail.com>
2023-09-15 20:54:03 +0545

    validation and token

--- betterbikes-server/src/utils/generateToken.ts
+++ betterbikes-server/src/utils/generateToken.ts
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId: string) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET as string,{
-    expiresIn: '1d'
+    expiresIn: process.env.JWT_EXPIRY_TIME
   });
 };
 
 export const generateRefreshToken = (userId: string) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET as string,{
-    expiresIn: '7d'
+    expiresIn: process.env.JWT_REFRESH_EXPIRY_TIME
   });
 }
--- betterbikes-server/src/validation/authValidation.ts
+++ betterbikes-server/src/validation/authValidation.ts
@@ -1,23 +1,71 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.string().nonempty("Email is required").email(),
-    password:z.string().nonempty("Password is required").min(6, "Password must be atleast 6 characters").max(100)
-})
+  email: z.string({
+    required_error: "Email is required",
+    invalid_type_error: "Invalid Email",
+  }),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Invalid Password",
+    })
+    .min(6, "Password must be atleast 6 characters")
+    .max(100),
+});
 
+export type LoginSchemaType = z.infer<typeof loginSchema>;
 
-export type LoginSchemaType = z.infer<typeof loginSchema>
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
 
-export const registerSchema = z.object({
-    fullName:z.string().nonempty("Name cannot be empty").min(1).max(30),
-    email: z.string().nonempty("Email cannopt be empty").email(),
-    password: z.string().nonempty("Password cannot be empty").min(6, "Password must be atleast 6 characters").max(100),
-    confirmPassword: z.string().nonempty("Confirm Password cannot be empty").min(6, "Password must be atleast 6 characters").max(100),
-})
-.refine((data) => data.password === data.confirmPassword, {
+export const registerSchema = z
+  .object({
+    fullName: z
+      .string({
+        required_error: "Name cannot be empty",
+        invalid_type_error: "Invalid Name",
+      })
+      .min(1)
+      .max(30),
+    email: z
+      .string({
+        required_error: "Email cannopt be empty",
+        invalid_type_error: "Invalid Email",
+      })
+      .email(),
+    password: z
+      .string({
+        required_error: "Password cannot be empty",
+        invalid_type_error: "Invalid Password",
+      })
+      .min(6, "Password must be atleast 6 characters")
+      .max(100),
+    confirmPassword: z
+      .string({
+        required_error: "Confirm Password cannot be empty",
+        invalid_type_error: "Invalid Confirm Password",
+      })
+      .min(6, "Password must be atleast 6 characters")
+      .max(100),
+    phone: z
+      .string({
+        required_error: "Phone cannot be empty",
+        invalid_type_error: "Invalid Phone",
+      })
+      .regex(phoneRegex, "Invalid Phone Number!"),
+    address: z
+      .string({
+        required_error: "Address Field Required",
+        invalid_type_error: "Invalid Address",
+      })
+
+      .min(3, "Minimum 3 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords must match",
     path: ["confirmPassword"],
   });
 
-
-export type RegisterSchemaType = z.infer<typeof registerSchema>
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
