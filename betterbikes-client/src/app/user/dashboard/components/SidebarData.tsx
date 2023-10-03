a49d2df2f4725880048e30674f00bb1be1c67ecd
tyzrex <sulavbaral58@gmail.com>
2023-10-03 22:57:51 +0545

    changes

--- betterbikes-client/src/app/user/dashboard/components/SidebarData.tsx
+++ betterbikes-client/src/app/user/dashboard/components/SidebarData.tsx
@@ -17,37 +17,37 @@ interface ISidebarItem {
 
 export const sidebarItems: ISidebarItem[] = [
   {
-    icon: <BiCategory className="w-6 h-6" />,
+    icon: <BiCategory className="w-6 h-6 text-main-accent" />,
     label: "Dasboard",
     href: "/user/dashboard",
   },
   {
-    icon: <PiPersonSimpleBikeFill className="w-6 h-6" />,
+    icon: <PiPersonSimpleBikeFill className="w-6 h-6 text-main-accent" />,
     label: "List Vehicle",
     href: "/user/listvehicle",
   },
   {
-    icon: <BiCategory className="w-6 h-6" />,
+    icon: <BiCategory className="w-6 h-6 text-main-accent" />,
     label: "My Vehicles",
     href: "/user/dashboard/Listings",
   },
   {
-    icon: <BsCartDash className="w-6 h-6" />,
+    icon: <BsCartDash className="w-6 h-6 text-main-accent" />,
     label: "Rent Requests",
     href: "/user/rent-requests",
   },
   {
-    icon: <PiMessengerLogo className="w-6 h-6" />,
+    icon: <PiMessengerLogo className="w-6 h-6 text-main-accent" />,
     label: "Messenger",
     href: "/user/messenger",
   },
   {
-    icon: <BiUserCircle className="w-6 h-6" />,
+    icon: <BiUserCircle className="w-6 h-6 text-main-accent" />,
     label: "Explore",
     href: "/user/dashboard/explore",
   },
   {
-    icon: <AiOutlineSetting className="w-6 h-6" />,
+    icon: <AiOutlineSetting className="w-6 h-6 text-main-accent" />,
     label: "Settings",
     href: "/dashboard/settings",
   },
